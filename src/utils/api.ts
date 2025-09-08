390700b34623fa433e34a57cbe203ccb557e203e
LettoKarvat <130711451+LettoKarvat@users.noreply.github.com>
2025-09-08 18:22:51 -0300

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -10,7 +10,7 @@ const VITE_API_URL = (import.meta as any).env?.VITE_API_URL as
 const BASE_URL =
   VITE_API_URL && VITE_API_URL.trim() !== ""
     ? VITE_API_URL
-    : "https://44d1522a900d.ngrok-free.app";
+    : "https://77ed93c6c304.ngrok-free.app";
 
 const isNgrok = /ngrok(-free)?\.app/i.test(BASE_URL);
 
